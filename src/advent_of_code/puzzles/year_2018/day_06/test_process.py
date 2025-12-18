5378478c47ac7590dc8cb1ea495332877b1a562e
Fadil Nohur <fadil.nohur@outlook.com>
2025-12-18 21:34:03 +0000

    Enhancements
    - Update test file error

--- src/advent_of_code/puzzles/year_2018/day_06/test_process.py
+++ src/advent_of_code/puzzles/year_2018/day_06/test_process.py
@@ -3,7 +3,7 @@ from advent_of_code.puzzles.year_2018.day_06 import process
 
 
 def test_example():
-    c = src.advent_of_code.puzzles.year_2018.day_06.process.ChronalCoods()
+    c = process.ChronalCoods()
     c.coords = [
         [1, 1],
         [1, 6],
@@ -16,5 +16,3 @@ def test_example():
     c.calculate_nearest_coords()
     assert c.retrieve_largest_finite_area() == 17
     assert c.calculate_region_size_close_to_all_coords(32) == 16
-
-pytest.main()
